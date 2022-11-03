eccfe59c01ffe4f90ccc860eb9a9386ecec30f31
CursoFlutterVictor <cursofluttervictor@gmail.com>
2022-11-03 11:13:19 +0100

    Inicio desarrollo

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,28 +5,6 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Column(
-      children: [
-        Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Expanded(
-              child: Container(
-            alignment: Alignment.center,
-            color: Colors.blueGrey,
-            child: Text("hola"),
-            height: 300,
-          ))
-        ]),
-        Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Expanded(
-              child: Container(
-            alignment: Alignment.center,
-            color: Colors.red,
-            child: Text("adios"),
-            height: 300,
-          ))
-        ])
-      ],
-    ));
+    return const Scaffold();
   }
 }
