19e007d957f48f9cd328cc8780939074e2a5b90c
CursoFlutterVictor <cursofluttervictor@gmail.com>
2022-11-10 13:18:23 +0100

    Firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,14 @@ import 'package:films_app/routes/app_pages.dart';
 import 'package:films_app/routes/app_routes.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
